5747a92d7e7c27dd501edcf33d0d3967f2553710
carlis <danahpa@gmail.com>
2021-08-19 13:32:50 -0500

    suma de precios por pelicula de la cartelera

--- peliculas.js
+++ peliculas.js
@@ -1,6 +1,26 @@
 let numeroPeliculas = 0;
 let personasPelicula = 0;
 
+class peliculaCartelera {
+  constructor(id, nombre, precio, plataforma, cine) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.plataforma = plataforma;
+    this.cine = cine;
+  }
+}
+
+const peli1 = new peliculaCartelera("peli1", "batman", 32, "netflix", false);
+const peli2 = new peliculaCartelera("peli2", "batman", 32, "netflix", false);
+const peli3 = new peliculaCartelera("peli3", "batman", 32, "netflix", false);
+const peli4 = new peliculaCartelera("peli4", "batman", 32, "netflix", false);
+const peli5 = new peliculaCartelera("peli5", "batman", 32, "netflix", false);
+const peli6 = new peliculaCartelera("peli6", "batman", 32, "netflix", false);
+const peli7 = new peliculaCartelera("peli7", "batman", 32, "netflix", false);
+const peli8 = new peliculaCartelera("peli8", "batman", 32, "netflix", false);
+const cartelera = [peli1, peli2, peli3, peli4, peli5, peli6, peli7, peli8];
+
 function guardarPelicula() {
   const textPeliculas = document.getElementById("PeliculasPorMes");
   numeroPeliculas = textPeliculas.value;
@@ -22,5 +42,18 @@ function contarPeliculas() {
 
   if (activatedMovies.length > numeroPeliculas) {
     return false;
+  } else {
+    precioEnElCine(activatedMovies);
+    return true;
   }
+}
+
+function precioEnElCine(_peliculas) {
+  const selectedMovies = cartelera.filter((movie) =>
+    _peliculas.includes(document.getElementById(movie.id))
+  );
+  const pricesMovies = selectedMovies.map((precio) => precio.precio);
+  const totalPerPerson =
+    _peliculas.length > 0 ? pricesMovies.reduce((a, b) => a + b) : 0;
+  console.log(totalPerPerson);
 }
